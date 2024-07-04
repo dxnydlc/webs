635e8f591fe9b42a0df2a12d2f3d6fde2e708bad
Dxny <dany@delacruz.pe>
2024-07-03 23:25:31 -0500

    logos

--- project-ccapaccorp/app/Views/layouts/principal.php
+++ project-ccapaccorp/app/Views/layouts/principal.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?= $this->renderSection('titulo') ?> | <?= $this->renderSection('header') ?></title>
     <!-- favicons Icons -->
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= esc($APP_URL) ?>assets/images/favicons/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= esc($APP_URL) ?>assets/images/favicons/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= esc($APP_URL) ?>assets/images/favicons/favicon-16x16.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= esc($APP_URL) ?>2024/favicon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= esc($APP_URL) ?>2024/favicon.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= esc($APP_URL) ?>2024/favicon.png" />
     <link rel="manifest" href="<?= esc($APP_URL) ?>assets/images/favicons/site.webmanifest" />
-    <meta name="description" content="Eduact HTML Template For Educaton & LMS" />
+    <meta name="description" content="CcapaCcorp Tiene como objetivo ampliar y fortalecer los conocimientos, aptitudes y habilidades de los profesionales que se desenvuelven diariamente en las empresas, brindándoles herramientas acordes a sus necesidades, para que logren un desempeño laboral exitoso y logren los objetivos institucionales." />
 
     <!-- fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -51,7 +51,7 @@
     <div class="custom-cursor__cursor-two"></div>
 
     <div class="preloader">
-        <div class="preloader__image" style="background-image: url(<?= esc($APP_URL) ?>assets/images/loader.png);"></div>
+        <div class="preloader__image" style="background-image: url(<?= esc($APP_URL) ?>2024/logo-04.png);"></div>
     </div>
     <!-- /.preloader -->
     <div class="page-wrapper">
