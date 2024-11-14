17d8e7d3627e30c21e68253ef9d545eb7764751a
Dxny <dany@delacruz.pe>
2024-11-14 11:34:35 -0500

    laboral

--- project-ccapaccorp/app/Views/seminario/laboral.php
+++ project-ccapaccorp/app/Views/seminario/laboral.php
@@ -81,10 +81,159 @@ main-header-two
             </div>
             <!-- ./col -->
         </div>
+        <!-- ./row -->
+
+        <div class="row" style="margin-top: 50px;" >
+
+            <div class="col-xl-6 wow fadeInUp" data-wow-delay="200ms">
+                <div class="course-three__item">
+                    <div class="course-three__thumb">
+                        <img src="/2024/29-09/seminario-laboral-01.jpg" alt="eduact">
+                    </div>
+                    <div class="course-three__content">
+                        <div class="course-three__time" >Laboral</div>
+                        <h4 class="course-three__title">
+                            <a href="<?= esc($APP_URL) ?>curso/11/conceptos-remunerativos-y-no-remunerativos" >
+                                Conceptos remunerativos y no remunerativos
+                            </a>
+                        </h4>
+                        <div class="course-three__bottom">
+                            <div class="course-three__author">
+                                <h5 class="course-three__author__name" >Jueves 24 de octubre</h5>
+                                <p class="course-three__author__designation" style="text-transform: none;" >De 7:00 p.m. a 10:00 p.m.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-xl-6 wow fadeInUp" data-wow-delay="200ms">
+                <div class="course-three__item">
+                    <div class="course-three__thumb">
+                        <img src="/2024/29-09/seminario-laboral-02.jpg" alt="eduact">
+                    </div>
+                    <div class="course-three__content">
+                        <div class="course-three__time" >Laboral</div>
+                        
+                        <h3 class="course-three__title">
+                            <a href="<?= esc($APP_URL) ?>curso/13/evita-contingecias-legales-ante-un-despido" >
+                                Evita contingecias legales ante un despido
+                            </a>
+                        </h3>
+
+                        <div class="course-three__bottom">
+                            <div class="course-three__author">
+                                <h5 class="course-three__author__name">Lunes 14 de octubre</h5>
+                                <p class="course-three__author__designation" style="text-transform: none;" >De 7:00 p.m. a 10:00 p.m.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ./col -->
+        </div>
 
         <!-- ./row -->
 
+        <div class="row">
+            <div class="col-xl-6 wow fadeInUp" data-wow-delay="200ms">
+                <div class="course-three__item">
+                    <div class="course-three__thumb">
+                        <img src="/2024/29-09/ultimos-precedentes-01.jpg" alt="eduact">
+                    </div>
+                    <div class="course-three__content">
+                        <div class="course-three__time" >Laboral</div>
+                        
+                        <h3 class="course-three__title">
+                            <a href="<?= esc($APP_URL) ?>curso/14/ultimos-precedentes-vinculantes-del-tribunal-de-fiscalizacion-laboral-de-sunafil-y-su-repercusion-en-los-empleadores" >
+                            Últimos precedentes vinculantes del tribunal de fiscalización laboral de sunafil y su repercusión en los empleadores.
+                            </a>
+                        </h3>
 
+                        <div class="course-three__bottom">
+                            <div class="course-three__author">
+                                <h5 class="course-three__author__name">Lunes 23 de septiembre</h5>
+                                <p class="course-three__author__designation" style="text-transform: none;" >De 7:00 p.m. a 10:00 p.m.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-xl-6 wow fadeInUp" data-wow-delay="200ms">
+                <div class="course-three__item">
+                    <div class="course-three__thumb">
+                        <img src="/2024/29-09/soma-01.jpg" alt="eduact">
+                    </div>
+                    <div class="course-three__content">
+                        <div class="course-three__time" >Laboral</div>
+                        
+                        <h3 class="course-three__title">
+                            <a href="<?= esc($APP_URL) ?>curso/15/Evita-multas-en-materia-de-seguridad-y-salud-en-el-trabajo" >
+                            Evita multas en materia de seguridad y salud en el trabajo
+                            </a>
+                        </h3>
+
+                        <div class="course-three__bottom">
+                            <div class="course-three__author">
+                                <h5 class="course-three__author__name">Jueves 05 de septiembre</h5>
+                                <p class="course-three__author__designation" style="text-transform: none;" >De 7:00 p.m. a 10:00 p.m.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ./col -->
+        </div>
+        <!-- ./row -->
+
+        <div class="row">
+            <div class="col-xl-6 wow fadeInUp" data-wow-delay="200ms">
+                <div class="course-three__item">
+                    <div class="course-three__thumb">
+                        <img src="/2024/29-09/seminario-igual-dad-salarial.jpg" alt="eduact">
+                    </div>
+                    <div class="course-three__content">
+                        <div class="course-three__time" >Laboral</div>
+                        
+                        <h3 class="course-three__title">
+                            <a href="<?= esc($APP_URL) ?>curso/16/elabora-e-implementa-la-igualdad-salarial-en-tu-empresa" >
+                                Elabora e implementa la igualdad salarial en tu empresa
+                            </a>
+                        </h3>
+
+                        <div class="course-three__bottom">
+                            <div class="course-three__author">
+                                <h5 class="course-three__author__name">Jueves 22 de agosto</h5>
+                                <p class="course-three__author__designation" style="text-transform: none;" >De 7:00 p.m. a 10:00 p.m.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-xl-6 wow fadeInUp" data-wow-delay="200ms">
+                <div class="course-three__item">
+                    <div class="course-three__thumb">
+                        <img src="/2024/29-09/seminario-inspeccion-sunafil-01.jpg" alt="eduact">
+                    </div>
+                    <div class="course-three__content">
+                        <div class="course-three__time" >Laboral</div>
+                        <h3 class="course-three__title">
+                            <a href="<?= esc($APP_URL) ?>curso/01/sabes-como-afrontar-exitosamente-una-inspeccion-de-SUNAFIL" >
+                            ¿Sabes cómo afrontar exitosamente una inspección de SUNAFIL?                       </a>
+                        </h3>
+                        <div class="course-three__bottom">
+                            <div class="course-three__author">
+                                <h5 class="course-three__author__name">Martes 16 de julio</h5>
+                                <p class="course-three__author__designation" style="text-transform: none;" >De 7:00 p.m. a 10:00 p.m.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- ./col -->
+        </div>
         <!-- ./row -->
 
         <div class="row">
